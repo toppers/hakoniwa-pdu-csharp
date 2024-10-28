c11f4ef72d8e00edf06ef768e741cdbe0e5a8850
Takashi Mori <kanetugu2015@gmail.com>
2024-10-28 15:22:57 +0900

    add project files

---
+++ hakoniwa-pdu-csharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hakoniwa_pdu_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
