29bb5a89f57145da054e0c3a0f0906741cc92b22
Takashi Mori <kanetugu2015@gmail.com>
2024-11-01 06:05:25 +0900

    renamed

--- dotnet.bash
+++ dotnet.bash
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [ $# -ne 1 ]
+then
+	echo "Usage: $0 {build|test}"
+	exit 1
+fi
+
+CMD=${1}
+
 cd hakoniwa
 dotnet build
 cd ..
@@ -8,6 +16,6 @@ cd tests
 for entry in pdu udp websocket
 do
 	cd hakoniwa.${entry}.test
-	dotnet build
+	dotnet ${CMD}
 	cd ..
 done
