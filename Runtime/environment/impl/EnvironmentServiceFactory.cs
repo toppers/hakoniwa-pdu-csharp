f3549e77e85d01ec77be29c56ce2033ba1484a29
Takashi Mori <kanetugu2015@gmail.com>
2024-11-03 08:47:20 +0900

    bug fixed

--- Runtime/environment/impl/EnvironmentServiceFactory.cs
+++ Runtime/environment/impl/EnvironmentServiceFactory.cs
@@ -2,70 +2,103 @@
 using hakoniwa.environment.impl.local;
 #if !NO_USE_UNITY
 using hakoniwa.environment.impl.unity;
+using UnityEngine;
 #endif
 using hakoniwa.environment.interfaces;
+#if !UNITY_WEBGL
 using Newtonsoft.Json;
+#endif
 using System.IO;
 
 namespace hakoniwa.environment.impl
 {
+    [System.Serializable]
     public class UdpConfig
     {
+#if UNITY_WEBGL
+        public int LocalPort;
+        public int RemotePort;
+        public string RemoteIPAddress;
+#else
         public int LocalPort { get; set; }
         public int RemotePort { get; set; }
         public string RemoteIPAddress { get; set; }
+#endif
     }
 
+    [System.Serializable]
     public class WebSocketConfig
     {
+#if UNITY_WEBGL
+        public string ServerURI;
+#else
         public string ServerURI { get; set; }
+#endif
     }
 
+    [System.Serializable]
     public class CommServiceConfig
     {
+#if UNITY_WEBGL
+        public UdpConfig Udp = new UdpConfig();
+        public WebSocketConfig WebSocket = new WebSocketConfig();
+#else
         public UdpConfig Udp { get; set; }
         public WebSocketConfig WebSocket { get; set; }
-    }    
+#endif
+    }
+
     public class EnvironmentServiceFactory
     {
-        static public IEnvironmentService Create(string service_type, string file_type="local", string path=".")
+        public static IEnvironmentService Create(string service_type, string file_type="local", string path=".")
         {
             return new EnvironmentService(service_type, file_type, path);
         }
     }
+
     public class EnvironmentService : IEnvironmentService
     {
-        IFileLoader file_loader;
-        ICommunicationService comm_service;
+        private IFileLoader file_loader;
+        private ICommunicationService comm_service;
 
         public void SetCommunication(ICommunicationService comm)
         {
             this.comm_service = comm;
         }
 
-        private CommServiceConfig loadCommServiceConfig(string path)
+        private CommServiceConfig LoadCommServiceConfig(string path)
         {
             string relative_path = path + "/" + "comm_service_config";
             string normalizedPath = relative_path.Replace('/', Path.DirectorySeparatorChar);
             string param = file_loader.LoadText(normalizedPath, ".json");
+
             try
             {
+#if UNITY_WEBGL
+                // WebGL用のJsonUtilityを使ってデシリアライズ
+                CommServiceConfig config = JsonUtility.FromJson<CommServiceConfig>(param);
+#else
+                // それ以外はNewtonsoft.Jsonを使用
                 CommServiceConfig config = JsonConvert.DeserializeObject<CommServiceConfig>(param);
+#endif
                 return config;
             }
-            catch (JsonException e)
+            catch (Exception e)
             {
                 Console.WriteLine($"JSONのパース中にエラーが発生しました: {e.Message}");
                 return null;
             }
         }
+
         public EnvironmentService(string service_type, string file_type, string path)
         {
-            if (file_type == "local") {
+            if (file_type == "local")
+            {
                 file_loader = new LocalFileLoader();
             }
 #if !NO_USE_UNITY
-            else if (file_type == "unity") {
+            else if (file_type == "unity")
+            {
                 file_loader = new ResourcesFileLoader();
             }
 #endif
@@ -75,24 +108,23 @@ namespace hakoniwa.environment.impl
             }
             else if (service_type == "udp")
             {
-                var config = loadCommServiceConfig(path);
+                var config = LoadCommServiceConfig(path);
                 comm_service = new UDPCommunicationService(config.Udp.LocalPort, config.Udp.RemoteIPAddress, config.Udp.RemotePort);
             }
             else if (service_type == "websocket_dotnet")
             {
-                var config = loadCommServiceConfig(path);
+                var config = LoadCommServiceConfig(path);
                 string serverUri = config.WebSocket.ServerURI;
                 comm_service = new WebSocketCommunicationService(serverUri);
             }
 #if !NO_USE_UNITY
             else if (service_type == "websocket_unity")
             {
-                var config = loadCommServiceConfig(path);
+                var config = LoadCommServiceConfig(path);
                 string serverUri = config.WebSocket.ServerURI;
                 comm_service = new WebGLSocketCommunicationService(serverUri);
             }
 #endif
-
         }
 
         public ICommunicationService GetCommunication()
@@ -105,5 +137,4 @@ namespace hakoniwa.environment.impl
             return file_loader;
         }
     }
-
 }
