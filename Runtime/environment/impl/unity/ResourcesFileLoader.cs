e44a533ad06899628b58a881d87862497471c28b
Takashi Mori <kanetugu2015@gmail.com>
2024-11-05 12:57:35 +0900

    bug fixed

--- Runtime/environment/impl/unity/ResourcesFileLoader.cs
+++ Runtime/environment/impl/unity/ResourcesFileLoader.cs
@@ -14,7 +14,10 @@ namespace hakoniwa.environment.impl.unity
         public string LoadText(string filePath, string extension = null)
         {
             // ./や先頭の/を削除した形でfullPathを作成
-            string fullPath = filePath.StartsWith("./") ? filePath.Substring(2) : filePath.TrimStart('/');
+            //string fullPath = filePath.StartsWith("./") ? filePath.Substring(2) : filePath.TrimStart('/');
+            string fullPath = filePath.StartsWith("." + Path.DirectorySeparatorChar.ToString())
+                ? filePath.Substring(2)
+                : filePath.TrimStart(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar);
             TextAsset textAsset = Resources.Load<TextAsset>(fullPath);
             if (textAsset == null)
             {
